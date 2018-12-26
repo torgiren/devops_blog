b3cdff9870e4cec6b00145f6fe9b5f3cde632726
Marcin Fabrykowski <git@fabrykowski.pl>
2018-12-26 15:10:17 +0100

    pelican core

---
+++ content/test.md
@@ -0,0 +1,4 @@
+Title: test
+Date: 2018-12-26
+
+to jest test 
---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'TORGiren'
+SITENAME = 'TORGiren DevOpses Blog'
+#SITEURL = ''
+SITEURL = 'http://blog.fabrykowski.pl'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Warsaw'
+
+DEFAULT_LANG = 'pl'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/')
+        )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+PLUGINS = ['sitemap']
+PLUGIN_PATHS = ['pelican-plugins']
+
+EXTERNDED_SITEMAP_PLUGIN = {
+    'priotities': {
+        'index': 1.0,
+        'articles': 0.8,
+        'pages': 0.5,
+        'others': 0.4
+    },
+    'changefrequencies': {
+        'index': 'daily',
+        'articles': 'weekly',
+        'pages': 'monthly',
+        'others': 'monthly'
+    }
+}
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.8,
+        'indexes': 1,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'weekly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+
+THEME = 'pelican-themes/tuxlite_tbs'
