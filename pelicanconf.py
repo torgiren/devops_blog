3d7d4900de22c27763b0c39aeb4a12fc80ff7145
Marcin Fabrykowski <git@fabrykowski.pl>
2018-12-29 17:29:37 +0100

    linki do social i ogolne

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,14 +21,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/')
-        )
+LINKS = None
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+          ('YouTube TORGiren DevOpses', 'https://www.youtube.com/channel/UC_WIjAwEBoM5zvLQdbLrp7A'),
+         )
 
 DEFAULT_PAGINATION = 10
 
